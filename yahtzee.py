f1accdf6ee4dcfd986880c9d44c25c085b391058
seabasschang <34096922+seabasschang@users.noreply.github.com>
2017-12-18 15:47:19 -0500

    Updated Board
    The board is essentially done. The stats() can be further integrated into the Tkinter visuals, but the basics are the same. Removed dice since we decided to use singular dice classes.

--- yahtzee.py
+++ yahtzee.py
@@ -1,32 +1,3 @@
-from random import randint
-
-class Dice:
-	def __init__(self,):
-		#Holds all five dice as values
-		self.a = randint(1,6)
-		self.b = randint(1,6)
-		self.c = randint(1,6)
-		self.d = randint(1,6)
-		self.e = randint(1,6)
-
-	def value(self):
-		return (self.a, self.b, self.c, self.d, self.e)
-
-	def roll(self, rollnum):
-		#rollnum is the specific positions of dice being rolled
-		#Doesn't need to return an array, but the updated values will need to be called
-		if rollnum.count('a') == 1:
-			self.a = randint(1,6)
-		if rollnum.count('b') == 1:
-			self.b = randint(1,6)
-		if rollnum.count('c') == 1:
-			self.c = randint(1,6)
-		if rollnum.count('d') == 1:
-			self.d = randint(1,6)
-		if rollnum.count('e') == 1:
-			self.e = randint(1,6)
-
-
 class Board:
 	def __init__(self,):
 		self.score = 0
@@ -42,72 +13,107 @@ class Board:
 		self.smstraight = 0
 		self.lgstraight = 0
 		self.yahtzee = 0
+		self.chance = 0
 
 	def check(self, dicestate, choice):
 		#dicestate will be the list of dice values.
-		#choice will be the combo to check for.
-		#Checks if the current list of dice values satisfies a special combination.
-
+		#choice will be the combo to check for, (ie. "Small Straight').
+		#Will check if the current list of dice values satisfies a special combination.
 		choice_is_valid = False
 		while not choice_is_valid:
 			choice_is_valid = True
-			if choice.lower() == str('three of a kind') or choice.lower() == str('3 of a kind'):
-				valid = False
+			valid = False
+			value = None
+			if choice.lower() == 'aces' and self.aces == 0:
+				if dicestate.count(1) >= 1:
+					valid = False
+					value = dicestate.count(1)
+			elif choice.lower() == 'twos' and self.twos == 0:
+				if dicestate.count(2) >= 1:
+					valid = False
+					value = dicestate.count(2)*2
+			elif choice.lower() == 'threes' and self.twos == 0:
+				if dicestate.count(3) >= 1:
+					valid = False
+					value = dicestate.count(3)*3
+			elif choice.lower() == 'fours' and self.twos == 0:
+				if dicestate.count(4) >= 1:
+					valid = False
+					value = dicestate.count(4)*4
+			elif choice.lower() == 'fives' and self.twos == 0:
+				if dicestate.count(5) >= 1:
+					valid = False
+					value = dicestate.count(5)*5
+			elif choice.lower() == 'sixes' and self.twos == 0:
+				if dicestate.count(6) >= 1:
+					valid = False
+					value = dicestate.count(6)*6							
+			elif choice.lower() == str('three of a kind') or choice.lower() == str('3 of a kind') and self.threekind == 0:
 				for n in range(1,7):
 					if dicestate.count(n) >= 3:
 						valid = True
+						value = sum(dicestate)
 						break
-			elif choice.lower() == str('four of a kind') or choice.lower() == str('4 of a kind'):
-				valid = False
+			elif choice.lower() == str('four of a kind') or choice.lower() == str('4 of a kind') and self.fourkind == 0:
 				for n in range(1,7):
 					if dicestate.count(n) >= 4:
 						valid = True
+						value = sum(dicestate)
 						break
-			elif choice.lower() == str('full house'):
-				valid = False
+			elif choice.lower() == str('full house') and self.fullhouse == 0:
 				for n in range(1,7):
 					if dicestate.count(n) == 3:
 						for d in range(1,7):
 							if dicestate.count(d) == 2:						
-							valid = True
-							break
-			elif choice.lower() == str('small straight'):
+								valid = True
+								value = 25
+								break
+			elif choice.lower() == str('small straight') and self.smstraight == 0:
 				sequence = sorted(dicestate)
 				if [1, 2, 3, 4] in sequence or [2, 3, 4, 5] in sequence or [3, 4, 5, 6] in sequence:
 					valid = True
-			elif choice.lower() == str('large straight'):
-			elif choice.lower() == str('chance'):
+					value = 30
+			elif choice.lower() == str('large straight') and self.lgstraight == 0:
+				sequence = sorted(dicestate)
+				if [1, 2, 3, 4, 5] in sequence or [2, 3, 4, 5, 6] in sequence:
+					valid = True
+					value = 40
+			elif choice.lower() == str('yahtzee') and self.yahtzee == 0:
+				for n in range(1,7):
+					if dicestate.count(n) == 6:
+						valid = True
+						value = 50
+						break
+			elif choice.lower() == str('chance') and self.chance == 0:
+				valid = True
+				value = sum(dicestate)
 			else:
 				input("\nThat's not a valid Combo! Try again... (Press Enter)")
 				choice_is_valid = False
 				os.system('clear')
 
 		return (valid, value)
+		#Determines whether the inputted combo itself exists.
+		#Returns whether the dice is valid as a combo, and the produced value.
 
 	def stats(self):
 		self.score = self.aces + self.twos + self.threes + self.fours + self.fives + self.sixes + self.threekind 
 		+ self.fourkind + self.fullhouse + self.smstraight + self.lgstraight + self.yahtzee
-		print('Yahtzee: ')
+		print('Yahtzee The Very Good Game')
 		print('-------------------------')
-		print('Aces: '+str(self.nice)+)
-		print('Twos: ' +str(self.humor)+)
-		print('Threes: ' +str(self.salty)+)
-		print('Fours: ' +str(self.mad)+)
-		print('Fives: '+str(self.nice)+)
-		print('Sixes: ' +str(self.humor)+)
-		print('3 of a Kind: ' +str(self.salty)+)
-		print('4 of a Kind: ' +str(self.mad)+)
-		print('Full House: ' +str(self.salty)+)
-		print('Small Straight: ' +str(self.mad)+)
-		print('Large Straight: ' +str(self.salty)+)
-		print('YAHTZEE: ' +str(self.mad)+)
-		print('Chance: ' +str(self.salty)+)
+		print('Aces: '+str(self.aces))
+		print('Twos: ' +str(self.twos))
+		print('Threes: ' +str(self.threes))
+		print('Fours: ' +str(self.fours))
+		print('Fives: '+str(self.fives))
+		print('Sixes: ' +str(self.sixes))
+		print('3 of a Kind: ' +str(self.threekind))
+		print('4 of a Kind: ' +str(self.fourkind))
+		print('Full House: ' +str(self.fullhouse))
+		print('Small Straight: ' +str(self.smstraight))
+		print('Large Straight: ' +str(self.lgstraight))
+		print('YAHTZEE: ' +str(self.yahtzee))
+		print('Chance: ' +str(self.chance))
 		print('-------------------------')
-		print('Total: '+self.score+'')
-		print('Quote: Ask Ventre for a quote on how he feels.')
-		print('Conduct: Play for Maestro Ventre to improve (or worsen) his mood!')
-		print('Nothing: Or play for him another day...')
+		print('Total = '+str(self.score)+'')
 		print('-------------------------')
-
-
-input('Roll which dice?: ')
