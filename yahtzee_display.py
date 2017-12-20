c3d6fb43eb7853393b6580bf224fd94194cb2962
wgreve20 <wgreve20@choate.edu>
2017-12-20 09:47:13 -0500

    comments 1.0

--- yahtzee_display.py
+++ yahtzee_display.py
@@ -1,5 +1,3 @@
-
-
 import random, sys
 import tkinter as tk
 
@@ -12,7 +10,7 @@ frame = tk.Frame(root)
 counter = 13
 
 
-class Dice:
+class Dice: #This class is simply a dice with the ability to be rolled, and which holds its current value.
 	def __init__(self, sides):
 		self.sides = sides
 		self.value = 0
@@ -20,13 +18,14 @@ class Dice:
 	def roll(self):
 		self.value = random.randint(1,self.sides)
 
-def returnState():
+def returnState(): #This function iterates through all of the dice and returns the values of all 5.
 	y = []
 	for i in x:
 		y.append(i.dice.value)
 	return(y)
 
-class square:
+class square: #This class is a button which can be selected or unselected, and which stores
+#a dice. 
 
 	def __init__(self, x):
 		self.dice = Dice(6)
@@ -46,8 +45,8 @@ class square:
 		self.box['text'] = self.dice.value
 		frame.pack()
 
-class reroll:
-	def __init__(self):
+class reroll: #this class is the button which rerolls the selected dice.
+	def __init__(self): 
 		self.box = tk.Button(frame, text = 'Reroll', width = 4, command = self.active)
 		self.box.grid(row = 1, column = 6)
 		self.rolls = 2
@@ -63,7 +62,9 @@ class reroll:
 			print('You are out of rolls')
 
 
-class Score:
+class Score: #This class is the button which allows you to score your current dice. 
+	#When clicked it prompts the user to input, in test, how they would like their dice to be scored.
+	#It then calls B.check, which checks if that score method is allowed, and returns the value if it is.
 	def __init__(self):
 		self.box = tk.Button(frame, text = 'Score', width = 4, command = self.active)
 		self.box.grid(row = 1, column = 3)
@@ -80,26 +81,23 @@ class Score:
 			y.rolls = 2
 		B.stats()
 		if counter <= 0:
-
 			sys.exit('Thanks for playing!')
 
 
 
-
+#Initializes all 5 dice buttons and stores them in an array.
 x = [square(i) for i in range(5)]
-
+#Rolls the dice the first time
 for i in x:
 	i.roll()
-
+#Initializes the reroll button
 y = reroll()
-
+#Initializes the score button
 z = Score()
-
+#Initializes the Board, which holds score information and processess score attempts
 B = Board()
-
+#calls B.Stats, which shows all relevent score information and tells the user how many rounds are left.
 B.stats()
+#Runs the program
 frame.pack()
-
-
-
 root.mainloop()
